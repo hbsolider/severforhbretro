89905f13eff40f18ea476a3015801584082c0521
HoaiBao <rorolo2011@gmail.com>
2020-10-29 16:02:24 +0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const app = express();
 const db = require("./database/connect");
 const cors = require("cors");
-app.use(cors({ allowedHeaders: ["Content-Type", "application/json"] }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
